7e960cb91d63a422fdac150e39d53ba07a311272
Max McKinney <maxgames101@gmail.com>
2020-09-12 16:00:02 -0500

    Initial Commit

---
+++ SimpleCounter WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  SimpleCounter WatchKit Extension
+//
+//  Created by Max McKinney on 9/12/20.
+//  Copyright © 2020 Max McKinney. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ SimpleCounter WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  SimpleCounter WatchKit Extension
+//
+//  Created by Max McKinney on 9/12/20.
+//  Copyright © 2020 Max McKinney. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
